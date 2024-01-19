0b8a7d95501804d3c1d8be1c82fa1cbf4f3efae5
icy1xtee <easton2904@mail.ru>
2024-01-19 10:45:27 +0300

    new commit 124622

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const helloFromAppJS = require('./app')
+const hello1 = require('./index22')
+
 console.log('Hello,world!');
 console.log('Hello from ilovegit branch');
 
 helloFromAppJS()
+
+const conflict = 'i want conflict to happen'
+
+console.log(conflict)
+
+hello1(conflict)
